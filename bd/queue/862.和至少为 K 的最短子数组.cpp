bf8bc9df2c5206ccb4cdb5ad98f0d4f399c5cd76
liuyizheng <liuyizheng@bigo.sg>
2021-06-16 14:34:38 +0800

    862

--- bd/queue/862.和至少为 K 的最短子数组.cpp
+++ bd/queue/862.和至少为 K 的最短子数组.cpp
@@ -1,9 +1,32 @@
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> sum(n+1, 0);
 
+        for (int i = 0; i < n; ++i) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+
+        deque<int> q;
+        int j = 0;
+        int res = n + 1;
+        while (j <= n) {
+            while (!q.empty() && sum[j] < sum[q.back()]) {
+                q.pop_back();
+            }
+            while (!q.empty() && sum[j] >= sum[q.front()] + k){
+                res = min(res, j - q.front());
+                q.pop_front();
+            }
+            q.push_back(j);
+            j++;
+        }
+
+        return res == n + 1 ? -1 : res;
     }
 };
