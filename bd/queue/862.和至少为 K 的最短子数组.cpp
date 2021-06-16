1596b5993dd943f2851b0598902b1a14619ba0e7
liuyizheng <liuyizheng@bigo.sg>
2021-06-16 14:13:39 +0800

    209

---
+++ bd/queue/862.和至少为 K 的最短子数组.cpp
@@ -0,0 +1,9 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+
+    }
+};
---
+++ bd/slidewindow/209.长度最小的子数组.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int minSubArrayLen(int target, vector<int> &nums)
+    {
+        int left = 0, right = 0, sum = 0, len = INT_MAX;
+        while (right < nums.size())
+        {
+            sum += nums[right];
+            while (sum >= target)
+            {
+                len = min(len, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+
+            right++;
+        }
+        return len == INT_MAX ? 0 : len;
+    }
+};
