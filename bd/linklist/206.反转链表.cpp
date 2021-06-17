e193594faa2234fc0f876f419ca0ed4eea4f7a0f
liuyizheng <liuyizheng@bigo.sg>
2021-06-17 11:46:40 +0800

    206

---
+++ bd/linklist/206.反转链表.cpp
@@ -0,0 +1,20 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *cur = head;
+        while (cur) {
+            ListNode *next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+};
