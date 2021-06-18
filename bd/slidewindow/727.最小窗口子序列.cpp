0dc937cd01efc5af32d194b4bf0ea72ac75a9a97
liuyizheng <liuyizheng@bigo.sg>
2021-06-18 15:59:50 +0800

    727

--- bd/slidewindow/727.最小窗口子序列.cpp
+++ bd/slidewindow/727.最小窗口子序列.cpp
@@ -1,3 +1,4 @@
+#include<limits.h>
 #include<string>
 using namespace std;
 
@@ -6,6 +7,33 @@ public:
     string minWindow(string s1, string s2) {
         if (s1 == "" || s2 == "") return "";
 
-        
+        int l = 0, r = INT_MAX, i1 = 0, i2 = 0;
+        while (i1 < s1.size()) {
+            if (s1[i1] == s2[i2]) {
+                i2++;
+            }
+            if (i2 == s2.size()) {
+                int tmpr = i1;
+                i2--;
+
+                while (i2 >= 0) {
+                    if (s1[i1] == s2[i2]) {
+                        i2--;
+                    }
+                    i1--;
+                }
+                
+                i1++;
+                i2 = 0;
+
+                if (tmpr - i1 < r - l) {
+                    r = tmpr;
+                    l = i1;
+                }
+            }
+            i1++;
+        }
+
+        return r == INT_MAX ? "" : s1.substr(l, r-l+1);
     }
 };
