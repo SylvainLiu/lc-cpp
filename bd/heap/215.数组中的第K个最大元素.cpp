1bfe2be44155aabec5e0f459181d5474eb4c8612
liuyizheng <liuyizheng@bigo.sg>
2021-06-19 17:36:25 +0800

    215

--- bd/heap/215.数组中的第K个最大元素.cpp
+++ bd/heap/215.数组中的第K个最大元素.cpp
@@ -1,10 +1,24 @@
+#include <queue>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (q.size() < k) {
+                q.push(nums[i]);
+            }
+            else {
+                if (q.top() < nums[i]) {
+                    q.pop();
+                    q.push(nums[i]);
+                }
+            }
+        }
 
+        return q.top();
     }
 };
 
